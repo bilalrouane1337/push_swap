9f320f2a2e2d5e7c40c48bb30192c37a3f4466c5
bilalrouane1337 <bilal.rouane1337@gmail.com>
2025-12-31 18:11:19 +0100

    new

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: brouane <brouane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 16:53:12 by brouane           #+#    #+#             */
-/*   Updated: 2025/12/24 22:20:34 by brouane          ###   ########.fr       */
+/*   Updated: 2025/12/31 18:10:17 by brouane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ size_t	ft_strlen(const char *s)
 	return (len);
 }
 
-char	*ft_putword(char const *s, int i, int word_len)
+char	*ft_putwordd(char const *s, int i, int word_len)
 {
 	int	j;
     char *word = malloc(word_len + 1);
@@ -51,31 +51,71 @@ char **prezero_remover(int count, char **numbers)
             i++;
         while (numbers[max][i] == 48)
             i++;
-        array[j] = ft_putword(numbers[max], i, ft_strlen(numbers[max]));
+        array[j] = ft_putwordd(numbers[max], i, ft_strlen(numbers[max]));
         max++;
         j++;
     }
     return (array);
 }
 
+void first(char **argv, int argc)
+{
+    int i = 0;
+    int c = 0;
+    while(argc - 1 > i)
+    {
+        c += ft_count_words(*(argv + i), ' ');
+        i++;
+    }
+    char **array = (char **)malloc(sizeof(char *) * (c + 1));
+    i = 0;
+    int word = 0;
+    while(argc - 1 > i)
+    {
+        word = ft_split_c(*(argv + i), array, ' ', word);
+        printf("w:%d\n", word);
+        i++;
+    }
+    i = 0;
+    while(c > i)
+    {
+        printf("s%d: %s\n", i, array[i]);
+        i++;
+    }
+    printf("cc:%d\n", c);
+}
+
 int main(int argc, char **argv)
 {
     char **array;
-    if (argc > 1)
-    {
-        int max = 1;
-        while (argc > max)
-        {   
-           
-            if (error_checker(argv[max]))
-                write(1, "e", 1);
-            else
-                write(1, "g", 1);
-            write(1, "\n", 1);
-            max++;
-        }
-        array = prezero_remover(argc, argv);
-        printf("%s\n", array[0]);
-        // duplicate_checker()
-    }
+    if (argc == 1 || (argc == 2 && !**(argv + 1)))
+        return (1);
+    int max = 1;
+    first(argv + 1, argc);
+    
+    // while (argc > max)
+    // {   
+    //     printf("l:%d\n", argc);
+    //     array = ft_split(argv[max], ' ');
+    //     // while()
+    //     // {
+            
+    //         printf("s: %s\n", array[max - 1]);
+    //     // }
+    //     if (error_checker(array[max - 1]))
+    //         write(1, "e", 1);
+    //     else
+    //         write(1, "g", 1);
+    //     write(1, "\n", 1);
+    //     max++;
+    // }
+    // // array = prezero_remover(argc, argv);
+    // max = 0;
+    // printf("W:%d\n", words);
+    // while (words > max)
+    // {
+    //     printf("%s\n", array[max]);
+    //     max++;
+    // }
+    // duplicate_checker()
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: brouane <brouane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 16:49:59 by brouane           #+#    #+#             */
-/*   Updated: 2025/12/24 17:16:21 by brouane          ###   ########.fr       */
+/*   Updated: 2025/12/31 18:10:01 by brouane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int ft_split_c(char const *s, char **s2, char c, int word);
 char	**ft_split(char const *s, char c);
 int error_checker(char *str);
+int	ft_count_words(char const *str, char c);
+char	*ft_putword(char *word, char const *s, int i, int word_len);
 
 #endif
